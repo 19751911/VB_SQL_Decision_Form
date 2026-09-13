c6247390f9f8f8f0ab7fcb3d30eeb8da7aace4da
19751911 <sgalxsy3@gmail.com>
2026-09-13 19:47:08 +0200

    إضافة ملف البرنامج الرئيسي

---
+++ Program.vb
@@ -0,0 +1,13 @@
+Namespace VB_SQL_Decision_Form
+    Friend NotInheritable Class Program
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread()> _
+        Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Application.Run(New MainForm())
+        End Sub
+    End Class
+End Namespace
